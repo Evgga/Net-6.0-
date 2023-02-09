24e9732477a1f8fd0ff5fe6d979b87a88a2378db
Evgenia <evgga@mail.ru>
2023-02-09 17:58:52 +0900

    Добавили Random

--- Example004_Div/Program.cs
+++ Example004_Div/Program.cs
@@ -1,3 +1,6 @@
-﻿double numberA = 12;
-double numberB = 5;
-Console.WriteLine(numberA / numberB);
+﻿int numberA = new Random().Next(1, 10);//1,2,3,4...9
+Console.WriteLine(numberA);
+int numberB = new Random().Next(1, 10);
+Console.WriteLine(numberB);
+int result = numberA + numberB;
+Console.WriteLine(result);
